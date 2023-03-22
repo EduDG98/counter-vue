f540243968e0c62649e83116e92eccac4ef19ef0
Eduardo Diaz <edudug98@gmail.com>
2023-03-22 14:18:15 +0000

    create a counter in the same component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script >
+import CounterDisplay from './components/CounterDisplay.vue'
+export default {
+  name: 'App',
+  components: {
+    CounterDisplay
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <CounterDisplay></CounterDisplay>
+</template>
+
+<style ></style>
---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'CounterDisplay',
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="increment()">➕</button>
+    <span> {{ count }} </span>
+    <button @click="decrement">➖</button>
+</template>
+
+<style></style>
